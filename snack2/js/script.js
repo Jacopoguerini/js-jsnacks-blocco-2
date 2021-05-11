128bad01b8c33b06866a41a9452fc5d5db09126a
Jacopoguerini <jacopo.guerini1993@gmail.com>
2021-05-11 15:45:41 +0200

    Creazione array di oggetti per snack 2

---
+++ snack2/js/script.js
@@ -0,0 +1,34 @@
+/*Snack 2
+Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate inizialmente a 0.
+Generare numeri random al posto degli 0 nelle proprietà: punti fatti e falli subiti.
+Infine creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.*/
+
+var teams = [
+    {
+    name: "Inter",
+    points: 85,
+    fs: 388
+    },
+    {
+    name: "Atalanta",
+    points: 72,
+    fs: 441
+    },
+    {
+    name: "Milan",
+    points: 72,
+    fs: 436
+    },
+    {
+    name: "Napoli",
+    points: 70,
+    fs: 496
+    },
+    {
+    name: "Juventus",
+    points: 69,
+    fs: 437
+    },
+];
+console.log(teams);
