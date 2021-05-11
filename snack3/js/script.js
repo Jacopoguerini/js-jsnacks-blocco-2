f5c1a4b5cfa6af6793090c42a2c0a708cef3ea8e
Jacopoguerini <jacopo.guerini1993@gmail.com>
2021-05-11 18:04:38 +0200

    Modifiche commenti

--- snack3/js/script.js
+++ snack3/js/script.js
@@ -1,14 +1,6 @@
 /*Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b). 
 La funzione restituirà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.*/
 
-// function yyy(arr, n1, n2) {
-//     // trasromare arr in newArr
-//     // ['andre', 'jacopo','luca','xyz'] 1 3
-//     return newArr; // ['andre', 'jacopo']
-// }
-// var x = [4,6,1,2,3,4];
-// console.log( yyy(x, 1, 3) ); // --> 6,1,2
-
 
 var array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 console.log(array);
