2eb9bc83a7381068f1e82a2f0731cfcfe25f43b5
Jacopoguerini <jacopo.guerini1993@gmail.com>
2021-05-11 15:35:36 +0200

    Snack 1 completato e funzionante, si può snellire

--- snack1/js/script.js
+++ snack1/js/script.js
@@ -24,18 +24,28 @@ var bicycleList = [
     name: "Merida",
     weight: 9
     }
-]
+];
 
 console.log(bicycleList);
 
-function minimumWeight(array) {
+// Math.min(1,2,5,0)  --> 0
+// Math.min(3,14,16,33) --> 3
+// creazione array del peso delle biciclette per confronto
+var prop = [];
+for (var i = 0; i < bicycleList.length; i++) {
+    prop.push(bicycleList[i].weight);
+}
+console.log(prop);
 
-    var weightMin;
-    for (var i = 0; i < array.length; i++) {
-        weightMin = Math.min(array[i].weight);
+// identificazione del peso minore
+var weightMin = Math.min(...prop);
+console.log(weightMin);
+
+// ciclo per stampa delle proprietà della bici con peso minore
+for (var i = 0; i < bicycleList.length; i++) {
+    if (bicycleList[i].weight == weightMin) {
+        console.log(bicycleList[i]);
     }
-    return weightMin;
 }
 
-var lighterBicycle = minimumWeight(bicycleList);
-console.log(lighterBicycle);
+
